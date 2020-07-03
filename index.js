f5ec25650909b1380ae29b004064c04a0b3efdfd
JW6677 <thoggarth@outlook.com>
2020-07-03 22:54:28 +0100

    Added !memes, !info moved api key to json file. Also improved !opinion

--- index.js
+++ index.js
@@ -1,33 +1,54 @@
-const Discord = require ('discord.js')
-const fs = require("fs")
+const Discord = require ('discord.js');
+const fs = require("fs");
 const client = new Discord.Client();
-const token = 'Njk3MTM3NjYxOTY5ODI1ODk1.Xv0NFQ.r1_QjeBVRs2xTOKm2juTRj3-HUg';
+var today = new Date();
+var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+const version = 'Flow V1.0.1';
+const prefix = '!';
+
+//Loading secrets file
+const secretsFile = fs.readFileSync('secrets.json');
+const secrets = JSON.parse(secretsFile);
 
 // Opinion files / array setup
-var opinionIntroFile = fs.readFileSync("./opinion-intro.txt", "utf-8");
-var opinionIntro = opinionIntroFile.split("\n")
-var opinionsFile = fs.readFileSync("./opinions.txt", "utf-8");
-var opinions = opinionsFile.split("\n")
+const opinionsFile = fs.readFileSync("./opinions.txt", "utf-8");
+const opinions = opinionsFile.split("\n")
+
+// Meme file / array setup
+const memeFile = fs.readFileSync("./memes.txt", "utf-8");
+const memes = memeFile.split("\n")
 
 client.on('message', msg=>{
-    if(msg.content === "!opinion"){
-        var opinion1 = ""
-        var opinion2 = ""
-        var opinion = ""
-        var opinion1 = opinionIntro[Math.floor(Math.random() * opinionIntro.length)]
-        var opinion2 = opinions[Math.floor(Math.random() * opinions.length)]
-        console.log(opinion1)
-        console.log(opinion2)
-        console.log(msg.author.username, ", Giving the Opinion :")
-        console.log(opinion.concat(opinion1, opinion2))
-        msg.reply(opinion.concat(opinion1, opinion2))
+    let args = msg.content.substring(prefix.length).split(' ')
+    switch(args[0]){
+        case 'opinion':
+            let opinion = opinions[Math.floor(Math.random() * opinions.length)]
+            console.log(msg.author.username, ", Giving the Opinion :");
+            console.log(opinion)
+            msg.reply(opinion)
+        break
+        case 'info':
+            let embed = new Discord.MessageEmbed()
+            .setTitle('Flow Info')
+            .addField('Name:', 'Flow')
+            .addField('Version', version)
+            .addField('Last Update', '03/7/2020')
+            .setColor(0xF1C40F)
+            .setThumbnail('https://i.ibb.co/LJYVSv2/flow.jpg')
+            .setFooter(date);
+            msg.channel.send(embed)
+        break
+        case 'meme':
+            let meme = memes[Math.floor(Math.random() * memes.length)]
+            msg.reply(meme);
+        break
     }
 })
 
-
 console.log('Loading Bot...')
+console.log(date)
 client.on('ready', () => {
     console.log('Bot Loaded');
 })
 
-client.login(token);
+client.login(secrets.token);
